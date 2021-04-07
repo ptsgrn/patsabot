273aeca8a2702862d972bcc036db845ce188999b
Patsagorn Y <manpatsagorny@gmail.com>
2021-04-07 21:23:18 +0700

    migrate from JavaScript to TypeScript

---
+++ built/tasks/category_redirect.js
@@ -0,0 +1,9 @@
+"use strict";
+const log = require('./ainalbot/logger');
+const config = require('./aianlbot/config');
+const bot = require('./ainalbot/bot');
+const category_redirector = {
+    getCategory: function () {
+    }
+};
+module.exports = category_redirector;
