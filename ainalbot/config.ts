2cd42e9dd3ba8f5a503a82995eb59b96cc244728
Patsagorn Y <manpatsagorny@gmail.com>
2021-04-08 22:34:28 +0700

    update

--- ainalbot/config.ts
+++ ainalbot/config.ts
@@ -1,13 +1,32 @@
-import * as load from 'load-json-file'
 import fs from 'fs'
 import yargs from 'yargs/yargs'
 const argv = yargs(process.argv.slice(2))
-  .option('credential',{
+  // --help, --version
+  .alias('help', 'h')
+  .alias('version', 'v')
+  .locale(yargs().locale())
+  .usage('$0 <add|rm|show|all>')
+  // --credential
+  .config('credential', function (configPath:string) {
+    return [JSON.parse(fs.readFileSync(configPath, 'utf-8'))]
   })
-  .config('config', function (configPath) {
+  .default('credential', 'credentials.json')
+  // --config
+  .config('config', function (configPath:string) {
     return JSON.parse(fs.readFileSync(configPath, 'utf-8'))
   })
-  .default('config', '../config.json')
+  .default('config', 'config.json')
+  .describe({
+    'config':'Configuration file',
+    'credential': 'Credential key (i.e. OAuth key) to use with `config.site` or `--site`, file can be blank if you don\'t have to login',
+  })
+  // --debug
+  .option('debug',{
+    alias: 'd',
+    boolean: true,
+  })
+  .group(['config', 'credential'], 'Bot\'s Autherization')
+  .epilogue('Running to any issue? File a bug at <https://gitlab.com/ptsgrn/ainalbot/-/issues> or contact the owner at <https://w.wiki/JSB>')
   .argv
 
-console.log(argv)
+export default argv
--- index.ts
+++ index.ts
@@ -1,17 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import mwn = require('mwn')
 
-// const config = require('./ainalbot/config').userInfo()
+import config from './ainalbot/config'
 
-let log = require('./ainalbot/logger')('main')
-var config = require('./config.json')
-
-const CronJob = require('cron').CronJob
-
-console.error('Before job instantiation')
-const job = new CronJob('* * * * * *', function() {
-  const d = new Date()
-  log.error('At Ten Minutes:', d)
-})
-
-log.info('After job instantiation')
+console.log(config)
