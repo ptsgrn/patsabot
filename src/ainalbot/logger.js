77d224f604bb74ba154cc9ec60ab7eafc6c4a241
Patsagorn Y <manpatsagorny@gmail.com>
2021-10-22 00:32:17 +0700

    style(logger): delete ending whitespace

--- src/ainalbot/logger.js
+++ src/ainalbot/logger.js
@@ -9,22 +9,24 @@ addColors({
 })
 const logger = createLogger({
   levels: {
-    emerg: 0, 
-    alert: 1, 
-    crit: 2, 
-    error: 3, 
-    warning: 4, 
-    notice: 5, 
-    info: 6, 
+    emerg: 0,
+    alert: 1,
+    crit: 2,
+    error: 3,
+    warning: 4,
+    notice: 5,
+    info: 6,
     debug: 7,
     done: 6,
     scriptrun: 5,
     scriptdone: 5
   },
-  level: 'info',
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
   format: format.combine(
     format.timestamp(),
-    format.errors({ stack: true }),
+    format.errors({
+      stack: true
+    }),
     format.splat(),
     format.json()
   ),
