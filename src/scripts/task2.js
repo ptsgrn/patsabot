0f0bccd535610ceee7bad7503e073cf17d07105f
Patsagorn Y <manpatsagorny@gmail.com>
2021-10-22 13:26:56 +0700

    refactor:
    update runner

--- src/scripts/task2.js
+++ src/scripts/task2.js
@@ -1,13 +1,17 @@
-const bot = require('../ainalbot/bot')
-bot.request({
-  'action': 'query',
-  'format': 'json',
-  'meta': 'userinfo',
-})
-  .then(data => {
-    console.log(data.query)
-  })
-  .catch(err=>{
-    console.log(err)
-  })
-
+module.exports = {
+  id: 'task2',
+  name: 'test task 2',
+  run: function _run({bot, log}) { 
+    bot.request({
+      'action': 'query',
+      'format': 'json',
+      'meta': 'userinfo',
+    })
+      .then(data => {
+        log.log('info', data.query)
+      })
+      .catch(err => {
+        log.log('error', err)
+      })
+  }
+}
