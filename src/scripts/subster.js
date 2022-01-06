2ee6fe3738b9184b147750beba2fea4ccae8f116
Patsagorn Y <49602385+ptsgrn@users.noreply.github.com>
2022-01-06 12:35:38 +0700

    feat(subster):  using dictionary to store page that have to edit fix #6

--- src/scripts/subster.js
+++ src/scripts/subster.js
@@ -1,28 +1,71 @@
 // Copyright (c) 2021 Patsagorn Y.
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-let category = 'หมวดหมู่:แม่แบบที่ควรรวมผ่านเสมอ'
-let templateslist = []
+const config = {
+  category: 'หมวดหมู่:แม่แบบที่ควรรวมผ่านเสมอ',
+  worknamespaces: ['0']
+}
+
+let workingPage = {}
+
+async function foreachTemplatename(templatename, idx, { log, bot }) {
+  log.log('debug', 'script.foreachTemplatename.start %s', templatename)
+  bot.request({
+    'action': 'query',
+    'format': 'json',
+    'prop': 'transcludedin',
+    'titles': templatename,
+    'utf8': 1,
+    'formatversion': '2',
+    'tinamespace': config.worknamespaces.join('|')
+  })
+    .then((json) => {
+      // log.log('debug', 'script.foreachTemplatename.mass.get %o', json)
+      json.query.pages.forEach(pagename => {
+        workingPage[pagename.title+''] = []
+        workingPage[pagename.title+''].push(templatename+'')
+      })
+    })
+    .finally(() => {
+      log.log('debug', 'script.foreachTemplatename.end %s', templatename)
+    })
+}
+
+function pushPageToWorks(page, templatename) {
+
+}
 
 async function main({bot, log}) {
-  log.log('info', 'start')
+  let templateslist = []
   for await (let json of bot.continuedQueryGen({
     'action': 'query',
     'format': 'json',
     'list': 'categorymembers',
-    'cmtitle': category,
+    'cmtitle': config.category,
     'cmlimit': 'max'
   })) {
     templateslist = templateslist.concat(json.query.categorymembers.map((tem) => tem.title))
-    log.log('info', 'end')
+    log.log('debug', 'script.templatelist.get %o', templateslist)
+    log.log('debug', 'script.templatelist.foreach.start')
+    bot.seriesBatchOperation(
+      templateslist,
+      async (templatename, idx) => {
+        await foreachTemplatename(templatename, idx, { log, bot })
+      },
+      2000,
+      2
+    ).finally(()=>{
+      log.log('debug', 'script.templatelist.foreach.end')
+      log.log('debug', 'script.foreachTemplatename.get %o', workingPage)
+    })
   }
 }
 
 module.exports = {
   id: 'subster',
   name: 'auto subster',
-  desc: `subst: แม่แบบที่อยู่ในหมวดหมู่ ${category} โดยอัตโนมัติหากไม่ได้ทำ`,
+  desc: `subst: แม่แบบที่อยู่ในหมวดหมู่ ${config.category} โดยอัตโนมัติหากไม่ได้ทำ`,
   run: main
 }
