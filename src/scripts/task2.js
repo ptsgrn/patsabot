d94d3edebdb4e51e1e4143fd5cf970fece15b57c
Patsagorn Y <49602385+ptsgrn@users.noreply.github.com>
2022-01-06 12:35:41 +0700

    feat: introduce new format date with moment.js

--- src/scripts/task2.js
+++
@@ -1,6 +0,0 @@
-import { getPageTranscluding } from '../patsabot/apis.js'
-export const id = 'task2'
-export const name = 'test task 2'
-export const run = async function _run() {
-  console.log(await getPageTranscluding('แม่แบบ:uw-ublock'))
-}
