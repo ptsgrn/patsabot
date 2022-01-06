26a323fa6dc1005912c46a3751e12b480ac35dc3
Patsagorn Y <49602385+ptsgrn@users.noreply.github.com>
2022-01-06 12:35:39 +0700

    feat: add parser function from CeJS

---
+++ src/ainalbot/utils.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 Patsagorn Y.
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import 'cejs'
+import 'cejs/application/net/wiki/parser.js'
+
+// eslint-disable-next-line no-undef
+export const parse = CeL.wiki.parse
