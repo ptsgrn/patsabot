2b25a7b35feaaa33ea3d4d346c5588b23d1912d2
Patsagorn <49602385+ptsgrn@users.noreply.github.com>
2022-08-22 14:43:12 +0700

    migrate jobs file

--- toolforge.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-# Copyright (c) 2022 Patsagorn Y.
-# 
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-# afccat
-NODE_ENV=production ./src/patsabot/run.js afccat -d "$(date +"%Y-%m-%d")" > /dev/null 2>&1 &
-
