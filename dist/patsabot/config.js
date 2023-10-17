4d60ecbd467561dfc8f6c323db2e6a509da031d8
Patsagorn <patsabot.maintainers@toolforge.org>
2023-10-17 18:33:31 +0700

    feat: update many and rename

--- dist/patsabot/config.js
+++ dist/patsabot/config.js
@@ -1,80 +1,11 @@
-// Copyright (c) 2021 Patsagorn Y.
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-import { JobOption } from './jobsmanager.js';
 import { resolveRelativePath, parseJsonFile } from './utils.js';
-
-interface ConfigTasksData {
-  name: string;
-  script: string;
-  cron: string;
-}
-interface ConfigFile {
-  config: {
-    user: string;
-    credentials: string;
-    simulate: boolean;
-    siteUrl: string;
-  };
-  log: {
-    logdir: string;
-    debug: boolean;
-    debugprefix: string;
-    log: boolean;
-    autolog: string[];
-    excludes: string[];
-  };
-  replica: {
-    provider: string[];
-    host: string;
-    port: number;
-    database: string;
-  };
-  tasks: ConfigTasksData[];
-}
-
-interface CredentailFile {
-  username: string;
-  password: string;
-  botAPIAccessToken: string;
-  consumerToken: string;
-  consumerSecret: string;
-  accessToken: string;
-  accessSecret: string;
-  replica: {
-    username: string;
-    password: string;
-  };
-  irc: {
-    nickName: string;
-    username: string;
-    password: string;
-    realName: string;
-    server: string;
-    port: number;
-  };
-  githooksecret: string;
-  scripts: {
-    archive: {
-      key_salt: string;
-    };
-  };
-  discord: {
-    webhook: {
-      logger: string;
-    };
-  };
-}
-
 // these are bad idea, but I will fix it as I now how ('=-=)
-export let credentials: CredentailFile = parseJsonFile(
+export let credentials = parseJsonFile(
   resolveRelativePath(import.meta.url, '../../credentials.json')
 );
-export let config: ConfigFile = parseJsonFile(
+export let config = parseJsonFile(
   resolveRelativePath(import.meta.url, '../../config.json')
 );
-
 /**
  * processing current user informations
  */
@@ -92,7 +23,6 @@ export const user = {
 export const site = {
   siteUrl: config.config.siteUrl ?? 'https://th.wikipedia.org/w/api.php',
 };
-
 export const ircConfig = {
   /**
    * IRC server address
@@ -129,14 +59,12 @@ export const ircConfig = {
    */
   nickName: credentials?.irc?.nickName,
 };
-
 export const replicaCredentials = {
   /** database table username for autherization */
   username: credentials?.replica?.username,
   /** database table password for autherization */
   password: credentials?.replica?.password,
 };
-
 export const replicaConfig = {
   /**
    * database table host
@@ -172,7 +100,6 @@ export const replicaConfig = {
     config?.replica?.port ?? 3306
   }/${config?.replica?.database ?? 'thwiki_p'}`,
 };
-
-export const schedule: JobOption[] = parseJsonFile(
+export const schedule = parseJsonFile(
   resolveRelativePath(import.meta.url, '../../schedule.json')
 );
---
+++ dist/patsabot/discord-transport.js
@@ -0,0 +1,148 @@
+import Transport from 'winston-transport';
+import axios from 'axios';
+import os from 'os';
+import rateLimit from 'axios-rate-limit';
+const http = rateLimit(axios.create(), {
+  maxRequests: 1,
+  perMilliseconds: 1000,
+});
+/**
+ * Nextabit's Discord Transport for winston
+ */
+class DiscordTransport extends Transport {
+  constructor(opts) {
+    super(opts);
+    /** Log queue */
+    this.queue = [];
+    /** Helper function to retrieve url */
+    this.getUrl = () => {
+      return `https://discordapp.com/api/v6/webhooks/${this.id}/${this.token}`;
+    };
+    /**
+     * Initialize the transport to fetch Discord id and token
+     */
+    this.initialize = () => {
+      this.initialized = new Promise((resolve, reject) => {
+        const opts = {
+          url: this.webhook,
+          method: 'GET',
+          json: true,
+        };
+        http
+          .get(opts.url)
+          .then((response) => {
+            this.id = response.data.id;
+            this.token = response.data.token;
+            resolve();
+          })
+          .catch((err) => {
+            console.error(
+              `Could not connect to Discord Webhook at ${this.webhook}`
+            );
+            reject(err);
+          });
+      });
+    };
+    /**
+     * Sends log message to discord
+     */
+    this.sendToDiscord = async (info) => {
+      const postBody = {
+        content: undefined,
+        embeds: [
+          {
+            description: info.message,
+            color: DiscordTransport.COLORS[info.level],
+            fields: [],
+            timestamp: new Date().toISOString(),
+          },
+        ],
+      };
+      if (info.level === 'error' && info.error && info.error.stack) {
+        postBody.content = `\`\`\`${info.error.stack}\`\`\``;
+      }
+      if (this.defaultMeta) {
+        Object.keys(this.defaultMeta).forEach((key) => {
+          postBody.embeds[0].fields.push({
+            name: key,
+            value: this.defaultMeta[key],
+          });
+        });
+      }
+      if (info[Symbol.for('splat')]) {
+        Object.keys(info[Symbol.for('splat')][0]).forEach((key) => {
+          postBody.embeds[0].fields.push({
+            name: key,
+            value: serailizeToList(info[Symbol.for('splat')][0][key]),
+          });
+        });
+      }
+      function serailizeToList(obj) {
+        if (typeof obj === 'string') {
+          return obj;
+        }
+        if (Array.isArray(obj)) {
+          return '- ' + obj.join('\n- ');
+        }
+        return Object.keys(obj)
+          .map((key) => {
+            return `${key}: ${obj[key]}`;
+          })
+          .join('\n');
+      }
+      postBody.embeds[0].fields.push({
+        name: 'Host',
+        value: os.hostname(),
+      });
+      const options = {
+        url: this.getUrl(),
+        method: 'POST',
+        json: true,
+        body: postBody,
+      };
+      this.queue.push(options);
+      try {
+        // await request(options);
+        await http.post(options.url, options.body, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      } catch (err) {
+        console.error('Error sending to discord', err);
+      }
+    };
+    this.webhook = opts.webhook;
+    this.defaultMeta = opts.defaultMeta || {};
+    this.initialize();
+  }
+  /**
+   * Function exposed to winston to be called when logging messages
+   * @param info Log message from winston
+   * @param callback Callback to winston to complete the log
+   */
+  log(info, callback) {
+    if (info.discord !== false) {
+      setImmediate(() => {
+        this.initialized
+          .then(() => {
+            this.sendToDiscord(info);
+          })
+          .catch((err) => {
+            console.log('Error sending message to discord', err);
+          });
+      });
+    }
+    callback();
+  }
+}
+/** Available colors for discord messages */
+DiscordTransport.COLORS = {
+  error: 14362664,
+  warn: 16497928,
+  info: 2196944,
+  verbose: 6559689,
+  debug: 2196944,
+  silly: 2210373, // #21ba45
+};
+export default DiscordTransport;
