78e44c650e77bdd3f2fb51b068d064464c0d948b
Patsagorn <patsagorn_y@outlook.com>
2023-12-02 23:41:21 +0700

    fix: Update build script and package.json

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 CDIR=$(pwd)
 echo -e ">> ${BLUE}Current directory: ${NC}"
 echo $CDIR
+
 # color
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -17,26 +18,15 @@ echo -e ">> ${BLUE}Building web app...${NC}"
 cd $CDIR/web
 
 # install dependencies
-pnpm i
-pnpm format
-pnpm build
+npm i
+npm build
 
 echo -e ">> ${GREEN}Web app build complete${NC}"
 echo -e ">> ${BLUE}Building server and bot script...${NC}"
 
 cd $CDIR
-pnpm i
-pnpm build
+tsc
 
 echo -e ">> ${GREEN}Server and bot script build complete${NC}"
 
-# update git 
-echo -e ">> ${BLUE}Updating git...${NC}"
-git add .
-
-# ask for commit message
-echo -e ">> ${YELLOW}Enter commit message:${NC}"
-read commitMessage
-git commit -m "$commitMessage"
-git push origin
-git push github
+echo -e ">> ${BLUE}DONE${NC}"
