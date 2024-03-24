12d66319e5d179b07cf5bd60a4c8fdc06b422f12
Patsagorn <patsagorn_y@outlook.com>
2024-03-24 22:39:42 +0700

    Update python/bootstrap_venv.sh script***

--- python/bootstrap_venv.sh
+++ python/bootstrap_venv.sh
@@ -3,11 +3,13 @@
 # use bash strict mode
 # set -euo pipefail || exit 1
 
+rm -rf pyvenv
+
 # create the venv
-python3 -m venv ~/pyvenv
+python3 -m venv pyvenv
 
 # activate it
-source ~/pyvenv/bin/activate
+source pyvenv/bin/activate
 
 # upgrade pip inside the venv and add support for the wheel package format
 pip install -U pip wheel
