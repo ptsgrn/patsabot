365dc5907b3f3eabcc4ee4dbbb9bcba5e61dbe3f
Patsagorn <patsagorn_y@outlook.com>
2025-01-07 17:15:37 +0700

    feat: add UpdateStatusBot for updating bot status with user timestamp

---
+++ scripts/update-status.ts
@@ -0,0 +1,20 @@
+import { Bot } from '@core/bot';
+
+export default class UpdateStatusBot extends Bot {
+  public info: { id: string; name: string; description: string; } = {
+    id: '0',
+    description: 'อัปเดตสถานะบอต',
+    name: 'updatestatus',
+  }
+  constructor() {
+    super()
+  }
+  async run() {
+    await this.bot.save(
+      `ผู้ใช้:${this.config.bot.username}/timestamp`,
+      "{{subst:#timel:r}}",
+      "อัปเดตสถานะ"
+    );
+    this.log.info("Status updated")
+  }
+}
