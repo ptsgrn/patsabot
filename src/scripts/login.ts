8283bbf8e84a73817d38ce60ee39527e35ba785e
Patsagorn <patsagorn_y@outlook.com>
2026-05-12 14:55:00 +0700

    แก้ type error

--- src/scripts/login.ts
+++ src/scripts/login.ts
@@ -3,80 +3,144 @@ import chalk from "chalk";
 import moment from "moment";
 import { version } from "../../package.json";
 
+type UserInfo = {
+  id?: number;
+  name: string;
+  registrationdate?: string;
+  editcount?: number;
+  latestcontrib?: string;
+  groups: string[];
+  groupmembership?: string[];
+  messages?: {
+    black?: string[];
+  };
+  unreadcount?: number;
+};
+
+type SiteInfo = {
+  sitename: string;
+  wikiid: string;
+  time: string;
+  timezone: string;
+  timeoffset: number;
+};
+
 export default class Login extends Bot {
-	info: Bot["info"] = {
-		id: "login",
-		name: "Login",
-		description: "Get site and user info",
-	};
+  info: Bot["info"] = {
+    id: "login",
+    name: "Login",
+    description: "Get site and user info",
+  };
 
-	async run() {
-		const siteAndUserInfo = await this.bot.request({
-			action: "query",
-			format: "json",
-			meta: "siteinfo|userinfo",
-			siprop: "general",
-			uiprop: "*",
-		});
-		if (!siteAndUserInfo) {
-			console.error("Failed to get site and user info");
-			return;
-		}
-		const userinfo = siteAndUserInfo.query?.userinfo;
-		const siteinfo = siteAndUserInfo.query?.general;
-		console.info(`
-  ${chalk.white.underline.bold(`Patsabot v${version}`)}
-  ${chalk.white.underline("USER INFO")}
-  ${
-		userinfo.id
-			? chalk.green("Logged in as: ") +
-				userinfo.name +
-				"\n" +
-				chalk.green("  User regist date: ") +
-				moment(userinfo.registrationdate).fromNow() +
-				" (" +
-				userinfo.registrationdate +
-				")\n" +
-				chalk.green("  User ID: ") +
-				userinfo.id +
-				"\n" +
-				chalk.green("  Edit count(s): ") +
-				userinfo.editcount +
-				"\n" +
-				chalk.green("  Latest contrib: ") +
-				moment(userinfo.latestcontrib).fromNow() +
-				"\n"
-			: chalk.red(
-					"  [Not logged in]                                         ",
-				) +
-				chalk.grey.bgBlueBright("      ") +
-				"\n"
-	}  ${chalk.green("Is bot:")} ${
-		userinfo.groups.includes("bot")
-			? chalk.bgGreen(" BOT ")
-			: chalk.bgRed(" NO ")
-	}
-  ${chalk.green("User groups:")} ${userinfo.groups.join(", ")}
-  ${chalk.green("User groups membership:")} ${
-		userinfo.groupmembership ? userinfo.groupmembership.join(", ") : "-"
-	}
-  ${chalk.green("Unreaded messages:")} ${
-		userinfo.messages
-			? chalk.grey.bgBlueBright(userinfo.messages.black.join(", "))
-			: "-"
-	}
-  ${chalk.green("Unseen watchlist count:")} ${
-		userinfo.unreadcount ? userinfo.unreadcount : "-"
-	}
+  async run() {
+    const siteAndUserInfo = await this.bot.request({
+      action: "query",
+      format: "json",
+      meta: "siteinfo|userinfo",
+      siprop: "general",
+      uiprop: "*",
+    });
+    if (!siteAndUserInfo) {
+      console.error("Failed to get site and user info");
+      return;
+    }
+    const userinfo = siteAndUserInfo.query?.userinfo as UserInfo | undefined;
+    const siteinfo = siteAndUserInfo.query?.general as SiteInfo | undefined;
 
-  ${chalk.white.underline("SITE INFO")}
-  ${chalk.green("Site name:")}\t${siteinfo.sitename}
-  ${chalk.green("Wiki ID:")}\t${siteinfo.wikiid}
-  ${chalk.green("Site Api Url:")}\t${this.bot.defaultOptions.apiUrl}
-  ${chalk.green("Server Time:")}\t${siteinfo.time}
-  ${chalk.green("Time zone:")}\t${siteinfo.timezone} (UTC${
-		(siteinfo.timeoffset >= 0 ? "+" : "-") + siteinfo.timeoffset / 60
-	})
-`);
-	}
+    if (!userinfo || !siteinfo) {
+      console.error("Failed to get site and user info");
+      return;
+    }
+
+    console.info(this.renderLoginReport(userinfo, siteinfo));
+  }
+
+  private renderLoginReport(userinfo: UserInfo, siteinfo: SiteInfo) {
+    return [
+      "",
+      `  ${chalk.white.underline.bold(`Patsabot v${version}`)}`,
+      `  ${chalk.white.underline("USER INFO")}`,
+      ...this.renderUserInfo(userinfo),
+      "",
+      `  ${chalk.white.underline("SITE INFO")}`,
+      ...this.renderSiteInfo(siteinfo),
+      "",
+    ].join("\n");
+  }
+
+  private renderUserInfo(userinfo: UserInfo) {
+    return [
+      ...this.renderLoginStatus(userinfo),
+      `  ${this.label("Is bot:")} ${this.botBadge(userinfo.groups)}`,
+      `  ${this.label("User groups:")} ${userinfo.groups.join(", ")}`,
+      `  ${this.label("User groups membership:")} ${this.formatList(userinfo.groupmembership)}`,
+      `  ${this.label("Unreaded messages:")} ${this.formatUnreadMessages(userinfo)}`,
+      `  ${this.label("Unseen watchlist count:")} ${userinfo.unreadcount ?? "-"}`,
+    ];
+  }
+
+  private renderLoginStatus(userinfo: UserInfo) {
+    if (!userinfo.id) {
+      return [
+        `  ${chalk.red("  [Not logged in]                                         ")}${chalk.grey.bgBlueBright("      ")}`,
+      ];
+    }
+
+    return [
+      `  ${this.label("Logged in as: ")}${userinfo.name}`,
+      this.labelValue(
+        "  User regist date: ",
+        `${moment(userinfo.registrationdate).fromNow()} (${userinfo.registrationdate})`,
+      ),
+      this.labelValue("  User ID: ", userinfo.id),
+      this.labelValue("  Edit count(s): ", userinfo.editcount ?? "-"),
+      this.labelValue(
+        "  Latest contrib: ",
+        moment(userinfo.latestcontrib).fromNow(),
+      ),
+    ];
+  }
+
+  private renderSiteInfo(siteinfo: SiteInfo) {
+    return [
+      this.labelValue("Site name:", siteinfo.sitename),
+      this.labelValue("Wiki ID:", siteinfo.wikiid),
+      this.labelValue("Site Api Url:", this.bot.defaultOptions.apiUrl ?? "-"),
+      this.labelValue("Server Time:", siteinfo.time),
+      this.labelValue(
+        "Time zone:",
+        `${siteinfo.timezone} (${this.formatUtcOffset(siteinfo.timeoffset)})`,
+      ),
+    ];
+  }
+
+  private label(text: string) {
+    return chalk.green(text);
+  }
+
+  private labelValue(label: string, value: string | number) {
+    return `  ${this.label(label)}\t${value}`;
+  }
+
+  private botBadge(groups: string[]) {
+    return groups.includes("bot")
+      ? chalk.bgGreen(" BOT ")
+      : chalk.bgRed(" NO ");
+  }
+
+  private formatList(values?: string[]) {
+    return values?.length ? values.join(", ") : "-";
+  }
+
+  private formatUnreadMessages(userinfo: UserInfo) {
+    const messages = userinfo.messages?.black;
+    return messages?.length
+      ? chalk.grey.bgBlueBright(messages.join(", "))
+      : "-";
+  }
+
+  private formatUtcOffset(timeoffset: number) {
+    const sign = timeoffset >= 0 ? "+" : "-";
+    return `UTC${sign}${Math.abs(timeoffset) / 60}`;
+  }
 }
